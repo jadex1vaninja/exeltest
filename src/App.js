0f52b9e497eec4c9430837740b547cf939ee7ee7
jadex1vaninja <ivan.lazarev@jadexconsulting.com>
2021-06-23 00:41:30 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+// import { ExcelFile, ExcelSheet } from "react-export-excel";
+import ReactExport from "react-export-excel";
+
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
+
+const object = {
+  1: {
+    name_en: "Prevalence of drug use - general population",
+    name_ar: null,
+    values: [2.0254, 0.71, null, null, 2.0254],
+  },
+  141: {
+    name_en: "Human Freedom Index",
+    name_ar: null,
+    values: [5.29, null, 7.81, 7.05, 5.29],
+  },
+};
+const country = [
+  "Country 1",
+  "Country 2",
+  "Country 3",
+  "Country 4",
+  "Country 5",
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Download></Download>
       </header>
     </div>
+  );
+}
+function Download() {
+  return (
+    <ExcelFile>
+      <ExcelSheet data={Object.values(object)} name="Employees">
+        <ExcelColumn label="" value="name_en" />
+        {country.map((e, index) => {
+          return (
+            <ExcelColumn
+              label={e}
+              value={(col) => col.values[index] || "пришел налл"}
+            />
+          );
+        })}
+      </ExcelSheet>
+    </ExcelFile>
   );
 }
 
